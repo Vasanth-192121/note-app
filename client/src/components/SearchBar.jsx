e40524b9a8b955ccaa3d09a0a718f314ae526ce7
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-10-01 00:05:09 +0530

    Update SearchBar.jsx

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -66,6 +66,7 @@
 
 // export default SearchBar
 
+
 import React, { useState, useEffect, useRef } from 'react';
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
@@ -122,35 +123,3 @@ const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
 };
 
 export default SearchBar;
-
-    };
-  }, []);
-
-  return (
-    <div className='w-40 xs:w-80 flex items-center px-4 bg-slate-100 rounded-xl'>
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder={placeholder}
-        className='w-full text-xs bg-transparent py-[11px] outline-none'
-        value={value}
-        onChange={onChange}
-      />
-
-      {value && (
-        <IoMdClose
-          className='text-xl text-slate-500 cursor-pointer hover:text-black mr-3'
-          onClick={onClearSearch}
-        />
-      )}
-
-      <FaMagnifyingGlass
-        className='text-slate-400 cursor-pointer hover:text-black'
-        onClick={handleSearch}
-      />
-    </div>
-  );
-};
-
-export default SearchBar;
-
