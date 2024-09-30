e6ca4c8d923bddd09c1fc4836485c7d1b5f8b282
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-09-30 23:21:26 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,57 @@
+// import React, { useState } from 'react';
+// import ProfileInfo from './ProfileInfo';
+// import { useNavigate } from 'react-router-dom';
+// import SearchBar from './SearchBar';
+
+// const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+
+//   const [searchQuery, setSearchQuery] = useState("");
+
+//   const navigate = useNavigate();
+
+//   const onLogout = () => {
+//     localStorage.clear();
+//     navigate("/login");
+//   }
+
+//   const handleSearch = () => {
+//     if (searchQuery) {
+//       onSearchNote(searchQuery);
+//     }
+//   };
+
+//   const onClearSearch = () => {
+//     setSearchQuery("");
+//     handleClearSearch();
+//   };
+
+//   return (
+//     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+//         <h2 className='text-xl font-medium text-black py-2'>
+//             Notes
+//         </h2>
+
+//       <SearchBar
+//         value={searchQuery}
+//         onChange={({ target }) => {
+//           setSearchQuery(target.value);
+//         }}
+
+//         handleSearch={handleSearch}
+//         onClearSearch={onClearSearch}
+//       />
+        
+//       <ProfileInfo 
+//         userInfo={userInfo}
+//         onLogOut={onLogout}
+//       />
+
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
 import React, { useState } from 'react';
 import ProfileInfo from './ProfileInfo';
 import { useNavigate } from 'react-router-dom';
@@ -26,7 +80,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
   };
 
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+    <div className='bg-white flex items-center gap-[8%] md:gap-[20%] lg:gap-[25%] xl:gap-[30%] px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>
             Notes
         </h2>
