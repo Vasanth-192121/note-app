6b952d6a13ee0ee43f1d2234634d4df2dd94f04e
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-09-30 23:19:17 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,250 @@
+// import React, { useState, useEffect } from 'react';
+// import Navbar from "../components/Navbar";
+// import NoteCard from '../components/NoteCard';
+// import { MdAdd } from 'react-icons/md';
+// import AddEditNotes from './AddEditNotes';
+// import Modal from "react-modal";
+// import { useNavigate } from 'react-router-dom';
+// import { axiosInstance } from '../utils/axiosInstance';
+// import Toast from '../components/Toast';
+// import EmptyCard from '../components/EmptyCard';
+// import AddNotesImg from '../../src/assets/add-notes.svg';
+// import NoDataImg from "../../src/assets/no-data.svg";
+
+// const Home = () => {
+//   const [openAddEditModel, setOpenAddEditModel] = useState({
+//     isShown: false,
+//     type: "add",
+//     data: null,
+//   });
+
+//   const [showToastMsg, setShowToastMsg] = useState({
+//     isShown : false,
+//     type : "add",
+//     data : null
+//   });
+
+//   const handleCloseToast = () => {
+//     setShowToastMsg({
+//       isShown : false,
+//       message : ""
+//     });
+//   }
+
+//   const showToastMessage = (message, type) => {
+//     setShowToastMsg({
+//       isShown : true,
+//       message,
+//       type
+//     });
+//   };
+
+//   const [userInfo, setUserInfo] = useState(null);
+//   const [allNotes, setAllNotes] = useState([]);
+
+//   const [isSearch, setIsSearch] = useState(false);
+
+//   const navigate = useNavigate();
+
+//   const handleEdit = (noteDetails) => {
+//     setOpenAddEditModel({ isShown : true, data : noteDetails, type : "edit" });
+//   };
+
+//   // Get User Info
+  
+//   const getUserInfo = async () => {
+//     try {
+//       const response = await axiosInstance.get("/get-user");
+
+//       if (response.data && response.data.user) {
+//         setUserInfo(response.data.user);
+//       }
+//     } catch (error) {
+//       if (error.response && error.response.status === 401) {
+//         localStorage.clear();
+//         navigate("/login");
+//       }
+//     }
+//   };
+
+//   // Get All Notes
+
+//   const getAllNotes = async () => {
+//     try {
+//       const response = await axiosInstance.get("/get-all-notes");
+
+//       if (response.data && response.data.notes) {
+//         setAllNotes(response.data.notes);
+//       }
+//     } catch (error) {
+//       console.log("An unexpected error occurred. Please try again.");
+//     }
+//   };
+
+//   // Delete Note
+
+//   const deleteNote = async (data) => {
+
+//     const noteId = data._id;
+
+//     try {
+//       const response = await axiosInstance.delete("/delete-note/" + noteId);
+
+//       if (response.data && !response.data.error) {
+//           showToastMessage("Note Deleted Successfully" ,"delete");
+//           getAllNotes()
+//       }
+
+//   } catch (error) {
+//       if (
+//           error.response && 
+//           error.response.data && 
+//           error.response.data.message
+//       ) {
+//         console.log("An unexpected error occurred. Please try again.");
+//       } 
+//   }
+//   };
+
+//   // Search for a Note
+
+//   const onSearchNote = async (query) => {
+
+//     try {
+//       const response = await axiosInstance.get("/search-notes", {
+//         params : { query },
+//       });
+
+//       if (response.data && response.data.notes) {
+//         setIsSearch(true);
+//         setAllNotes(response.data.notes);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+    
+//   };
+
+//   // Update isPinned
+
+//   const updateIsPinned = async (noteData) => {
+
+//     const noteId = noteData._id;
+
+//     try {
+//         const response = await axiosInstance.put("/update-note-pinned/" + noteId, {
+//           "isPinned" : !noteData.isPinned
+//         });
+
+//         if (response.data && response.data.note) {
+//           // showToastMessage("Pinned Successfully");
+//           noteData.isPinned ? showToastMessage("Unpinned Successfully") : showToastMessage("Pinned Successfully");
+//           getAllNotes();
+//         }
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+//   };
+
+//   // Handle Clear Search
+
+//   const handleClearSearch = () => {
+//     setIsSearch(false);
+//     getAllNotes();
+//   };
+
+//   useEffect(() => {
+//     getAllNotes();
+//     getUserInfo();
+//   }, []);
+
+//   return (
+//     <>
+//       <Navbar userInfo={userInfo} onSearchNote={onSearchNote} handleClearSearch={handleClearSearch} />
+
+//       <div className='container mx-auto'>
+//         {allNotes.length > 0 ? ( <div className='grid grid-cols-3 gap-4 mt-8'>
+
+//           {
+//           allNotes.map((item, index) => (
+//             <NoteCard 
+//               key={item._id}
+//               title={item.title}
+//               date={item.createdOn}
+//               content={item.content}
+//               tags={item.tags}
+//               isPinned={item.isPinned}
+//               onEdit={() => {handleEdit(item)}}
+//               onDelete={() => {deleteNote(item)}}
+//               onPinNote={() => {updateIsPinned(item)}}
+//             />
+//           ))}
+//         </div> 
+//         ) : (
+//           <EmptyCard 
+//             imgScr={ isSearch ? NoDataImg : AddNotesImg} 
+//             message={ isSearch 
+//                 ? `Oops! No notes matching your search` 
+//                 : `Start creating your first note! Click the 'Add' button to 
+//                   jot down your thoughts, ideas and reminders. Let's get started!`
+//               }
+//           />
+//         )}
+//       </div>
+
+//       <button 
+//         className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' 
+//         onClick={() => {
+//           setOpenAddEditModel({
+//             isShown: true,
+//             type: "add",
+//             data: null
+//           });
+//         }}
+//       >
+//         <MdAdd className='text-[32px] text-white' />
+//       </button>
+
+//       <Modal 
+//         isOpen={openAddEditModel.isShown}
+//         onRequestClose={() => {}}
+//         style={{
+//           overlay: {
+//             backgroundColor: "rgba(0, 0, 0, 0.2)",
+//           },
+//         }}
+//         contentLabel=''
+//         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-visible"
+//       >
+//         <AddEditNotes 
+//           type={openAddEditModel.type}
+//           noteData={openAddEditModel.data}
+//           onClose={() => {
+//             setOpenAddEditModel({ 
+//               isShown: false, 
+//               type: "add", 
+//               data: null 
+//             });
+//           }}
+//           getAllNotes={getAllNotes}
+//           showToastMessage={showToastMessage}
+//         />
+//       </Modal>
+      
+//       <Toast 
+//         isShown={showToastMsg.isShown}
+//         message={showToastMsg.message}
+//         type={showToastMsg.type}
+//         onClose={handleCloseToast}
+//       />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
 import React, { useState, useEffect } from 'react';
 import Navbar from "../components/Navbar";
 import NoteCard from '../components/NoteCard';
@@ -164,7 +411,7 @@ const Home = () => {
       <Navbar userInfo={userInfo} onSearchNote={onSearchNote} handleClearSearch={handleClearSearch} />
 
       <div className='container mx-auto'>
-        {allNotes.length > 0 ? ( <div className='grid grid-cols-3 gap-4 mt-8'>
+        {allNotes.length > 0 ? ( <div className='grid grid-cols-2 xs:grid-cols-3 gap-4 mt-8'>
 
           {
           allNotes.map((item, index) => (
@@ -215,7 +462,7 @@ const Home = () => {
           },
         }}
         contentLabel=''
-        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-visible"
+        className="w-5/12 min-w-64 max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-visible"
       >
         <AddEditNotes 
           type={openAddEditModel.type}
