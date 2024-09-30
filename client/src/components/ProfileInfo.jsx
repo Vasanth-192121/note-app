a98b2c8f16e0cca41529e5706817ccb78b8e1540
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-09-30 23:21:03 +0530

    Update ProfileInfo.jsx

--- client/src/components/ProfileInfo.jsx
+++ client/src/components/ProfileInfo.jsx
@@ -2,6 +2,9 @@
 // import { getInitials } from '../utils/Helper';
 
 // const ProfileInfo = ({ userInfo, onLogOut }) => {
+//   if (!userInfo) {
+//     return null; // or a loading spinner, or any placeholder
+//   }
 
 //   return (
 //     <div className='flex items-center gap-3'>
@@ -9,7 +12,7 @@
 //         {getInitials(userInfo.fullName)}
 //       </div>
 //       <div>
-//         <p className='text-sm font-medium'>{ userInfo.fullName }</p>
+//         <p className='text-sm font-medium'>{userInfo.fullName}</p>
 //         <button 
 //           className='text-sm text-slate-700 underline'
 //           onClick={onLogOut}
@@ -29,24 +32,26 @@ import { getInitials } from '../utils/Helper';
 
 const ProfileInfo = ({ userInfo, onLogOut }) => {
   if (!userInfo) {
-    return null; // or a loading spinner, or any placeholder
+    return null;
   }
 
   return (
     <div className='flex items-center gap-3'>
+    <div className='hidden xs:flex items-center justify-center'>
       <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
         {getInitials(userInfo.fullName)}
       </div>
-      <div>
-        <p className='text-sm font-medium'>{userInfo.fullName}</p>
-        <button 
-          className='text-sm text-slate-700 underline'
-          onClick={onLogOut}
-        >
-          Logout
-        </button>
-      </div>
     </div>
+    <div>
+      <p className='text-sm font-medium'>{userInfo.fullName}</p>
+      <button 
+        className='text-sm text-slate-700 underline'
+        onClick={onLogOut}
+      >
+        Logout
+      </button>
+    </div>
+  </div>
   );
 };
 
