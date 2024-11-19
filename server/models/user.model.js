d6d466dba301aec2db06cb2e0adc07e532a502b4
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-11-19 20:32:41 +0530

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,26 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     fullName : { type : String },
+//     email : { type : String },
+//     password : { type : String },
+//     createdOn: { type: Date, default: Date.now },
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+// export default User;
+
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    fullName : { type : String },
-    email : { type : String },
-    password : { type : String },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     createdOn: { type: Date, default: Date.now },
+    otp: { type: String },
+    verified: { type: Boolean, default: false } // Add verified flag
 });
 
 const User = mongoose.model("User", userSchema);
