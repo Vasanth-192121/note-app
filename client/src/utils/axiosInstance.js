157c2951a4f54a75287e62850045cc559ea34286
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-11-29 21:29:56 +0530

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : "https://keeper-notes-app-server.vercel.app/",
+    baseURL : "https://note-app-server-seven.vercel.app/" || "https://keeper-notes-app-server.vercel.app/",
     timeout : 10000,
     headers : {
         "Content-Type" : "application/json"
