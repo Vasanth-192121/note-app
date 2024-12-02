6a8b7930f4184299aa2285620cbbf88e49b38c62
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-12-02 20:08:25 +0530

    Contrast ratio updated for accessibility

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      // Colors used in this project
-      colors: {
-        primary: "#2B85FF",
-        secondary: "3EF863E"
-      },
-      // Custom breakpoints
-      screens: {
-        'xs': '400px', // Custom breakpoint for 450px
-      },
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    // Colors used in this project
+    colors: {
+      primary: "#003DF5",
+      secondary: "3EF863E"
+    },
+    // Custom breakpoints
+    screens: {
+      'xs': '400px', // Custom breakpoint for 450px
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
