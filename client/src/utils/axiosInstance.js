f5f77b1fdba7f665dee17091a6f3ede0dfa152a5
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2024-12-07 21:28:46 +0530

    Login Error Fixing

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { REACT_APP_BASE_URL } from "./constants";
+// import { REACT_APP_BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-    baseURL: REACT_APP_BASE_URL,
-    // baseURL : "https://note-app-server-seven.vercel.app/",
+    // baseURL: REACT_APP_BASE_URL,
+    baseURL : "https://note-app-server-seven.vercel.app/",
     // baseURL : "http://localhost:3000",
     // baseURL : "https://keeper-notes-app-server.vercel.app/",
     timeout : 10000,
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const REACT_APP_BASE_URL = "http://localhost:3000/";
+// export const REACT_APP_BASE_URL = "http://localhost:3000/";
 
 // export const REACT_APP_BASE_URL = "https://note-app-server-seven.vercel.app/";
 
