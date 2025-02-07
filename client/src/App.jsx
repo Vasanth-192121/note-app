4c5bf9885bf4f755416d96b5c08c9172a1862561
Vasanthamohan R <140964781+Vasanth-192121@users.noreply.github.com>
2025-02-07 11:16:28 +0530

    Rewrite the Vercel Json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 const RouteComponent = () => {
   return (
@@ -11,15 +16,17 @@ const RouteComponent = () => {
       <SpeedInsights />
       <Router>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/dashboard' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* Add a catch-all route for 404s */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </>
   );
-}
+};
 
 const App = () => {
   return (
@@ -27,6 +34,6 @@ const App = () => {
       <RouteComponent />
     </div>
   );
-}
+};
 
 export default App;
